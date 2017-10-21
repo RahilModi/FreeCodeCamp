c738da88842b600d5000ff56508f9b0b9a604abf
RahilModi <rmodi10@gmail.com>
2017-10-21 00:28:46 -0700

    modifications

--- Timestamp/app.js
+++ Timestamp/app.js
@@ -29,7 +29,7 @@ app.get('/:dateVal',(req,res)=>{
     }else{
         res.send({
             unix: dateVal,
-            time: moment(new Date(dateVal*1000)).format("MMMM DD, YYYY")
+            natural: moment(new Date(dateVal*1000)).format("MMMM DD, YYYY")
         });
     }
    
